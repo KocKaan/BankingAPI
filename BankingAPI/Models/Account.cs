52b9fbcceefb104e69d977a015fe9c95760b7439
KocKaan <kk4291@nyu.edu>
2022-04-19 19:39:09 -0400

    finished account model

--- BankingAPI/Models/Account.cs
+++ BankingAPI/Models/Account.cs
@@ -3,8 +3,38 @@ namespace BankingAPI.Models
 {
     public class Account
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal CurrentAccountBalance { get; set; }
+        public AccountType AccountType { get; set; }
+
+        //generate account number here 
+        public string AccountNumberGenerated { get; set; }
+
+        //storing the hash and salt of the Account Transaction pin
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+
+
+        //to create random account number;
+        Random rand = new Random();
         public Account()
         {
+            AccountNumberGenerated = Convert.ToString((long)Math.Floor(rand.NextDouble() * 9_000_000_000L + 1_000_000_000L));
         }
+
+    }
+
+    public enum AccountType
+    {
+        Savings,
+        Current,
+        Corporate,
+        Government
     }
 }
